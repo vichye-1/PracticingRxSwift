35e34ab31ae05093a52b72a95cbe1f137dbefe99
knockknock <yangban11111@gmail.com>
2024-07-30 18:22:35 +0900

    RxSwift로 UISwitch 구현

--- PracticingRxSwift/UISwitchViewController.swift
+++ PracticingRxSwift/UISwitchViewController.swift
@@ -11,9 +11,34 @@ import RxCocoa
 import SnapKit
 
 final class UISwitchViewController: BaseViewController {
+    private let disposeBag = DisposeBag()
+    
     private let simpleSwitch = UISwitch()
+    private let simpleLabel = UILabel()
     
     override func configureView() {
-        
+        view.addSubview(simpleSwitch)
+        view.addSubview(simpleLabel)
+        simpleSwitch.snp.makeConstraints { make in
+            make.centerX.centerY.equalTo(view.safeAreaLayoutGuide)
+        }
+        simpleLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.height.equalTo(80)
+        }
+        simpleLabel.backgroundColor = .lightGray
+    }
+    
+    // MARK: - view LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setSwitch()
+    }
+    
+    private func setSwitch() {
+        Observable.of(false)
+            .bind(to: simpleSwitch.rx.isOn)
+            .disposed(by: disposeBag)
     }
 }
